97b3eef32257974f6097abce1ebe6f60f6b46a80
James Strachan <james.strachan@gmail.com>
2020-09-29 14:29:34 +0100

    fix: handle pipeline status better
    and merging nicely the PipelineRun and PipelineActivity resources

--- pkg/common/pipelines/pipelines.go
+++ pkg/common/pipelines/pipelines.go
@@ -5,8 +5,11 @@ import (
 	"strings"
 
 	v1 "github.com/jenkins-x/jx-api/pkg/apis/jenkins.io/v1"
+	"github.com/jenkins-x/jx-helpers/pkg/kube/activities"
+	"github.com/jenkins-x/jx-helpers/pkg/kube/naming"
 	"github.com/jenkins-x/jx-logging/pkg/log"
 	"github.com/jenkins-x/octant-jx/pkg/common/viewhelpers"
+	"github.com/pkg/errors"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"github.com/vmware-tanzu/octant/pkg/plugin/service"
 	"github.com/vmware-tanzu/octant/pkg/store"
@@ -34,26 +37,33 @@ func GetPipelines(ctx context.Context, client service.Dashboard, ns string) ([]*
 		}
 	}
 
-	if len(paList) == 0 {
-		dl, err = client.List(ctx, store.Key{
-			APIVersion: "tekton.dev/v1beta1",
-			Kind:       "PipelineRun",
-			Namespace:  ns,
-		})
-		if dl != nil {
-			for k, v := range dl.Items {
-				pr := &v1beta1.PipelineRun{}
-				err := viewhelpers.ToStructured(&dl.Items[k], pr)
-				if err != nil {
-					log.Logger().Infof("failed to convert to PipelineActivity for %s: %s", v.GetName(), err.Error())
-					continue
-				}
+	dl, err = client.List(ctx, store.Key{
+		APIVersion: "tekton.dev/v1beta1",
+		Kind:       "PipelineRun",
+		Namespace:  ns,
+	})
+	if dl != nil {
+		for k, v := range dl.Items {
+			pr := &v1beta1.PipelineRun{}
+			err := viewhelpers.ToStructured(&dl.Items[k], pr)
+			if err != nil {
+				log.Logger().Infof("failed to convert to PipelineActivity for %s: %s", v.GetName(), err.Error())
+				continue
+			}
+			pr.Name = ToPipelineActivityName(pr, paList)
 
-				pa := ToPipelineActivity(pr)
-				if pa != nil {
-					paList = append(paList, pa)
+			var pa *v1.PipelineActivity
+			for i, r := range paList {
+				if r.Name == pr.Name {
+					pa = paList[i]
+					break
 				}
 			}
+			if pa == nil {
+				pa = &v1.PipelineActivity{}
+				paList = append(paList, pa)
+			}
+			ToPipelineActivity(pr, pa)
 		}
 	}
 	return paList, err
@@ -62,24 +72,96 @@ func GetPipelines(ctx context.Context, client service.Dashboard, ns string) ([]*
 func GetPipeline(ctx context.Context, client service.Dashboard, ns, name string) (*v1.PipelineActivity, error) {
 	u, err := viewhelpers.GetResourceByName(ctx, client, "jenkins.io/v1", "PipelineActivity", name, ns)
 	if err != nil {
-		u2, err2 := viewhelpers.GetResourceByName(ctx, client, "tekton.dev/v1beta1", "PipelineRun", name, ns)
+		paList, err2 := GetPipelines(ctx, client, ns)
 		if err2 == nil {
-			pr := &v1beta1.PipelineRun{}
-			err2 = viewhelpers.ToStructured(u2, pr)
-			if err2 == nil {
-				pa := ToPipelineActivity(pr)
-				return pa, nil
+			for _, pa := range paList {
+				if pa.Name == name {
+					return pa, nil
+				}
 			}
 		}
 		return nil, nil
 	}
-	return viewhelpers.ToPipelineActivity(u)
+	pa, err := viewhelpers.ToPipelineActivity(u)
+	if err != nil {
+		return pa, errors.Wrapf(err, "failed to load PipelineActivity")
+	}
+
+	dl, err := client.List(ctx, store.Key{
+		APIVersion: "tekton.dev/v1beta1",
+		Kind:       "PipelineRun",
+		Namespace:  ns,
+	})
+	paList := []*v1.PipelineActivity{pa}
+	if dl != nil {
+		for k, v := range dl.Items {
+			pr := &v1beta1.PipelineRun{}
+			err := viewhelpers.ToStructured(&dl.Items[k], pr)
+			if err != nil {
+				log.Logger().Infof("failed to convert to PipelineActivity for %s: %s", v.GetName(), err.Error())
+				continue
+			}
+
+			pr.Name = ToPipelineActivityName(pr, paList)
+			if pr.Name == name {
+				ToPipelineActivity(pr, pa)
+				return pa, nil
+			}
+		}
+	}
+	return pa, nil
 }
 
-func ToPipelineActivity(pr *v1beta1.PipelineRun) *v1.PipelineActivity {
+func ToPipelineActivityName(pr *v1beta1.PipelineRun, paList []*v1.PipelineActivity) string {
+	labels := pr.Labels
+	if labels == nil {
+		return pr.Name
+	}
+	build := labels["build"]
+	owner := labels["lighthouse.jenkins-x.io/refs.org"]
+	if owner == "" {
+		owner = labels["owner"]
+	}
+	repository := labels["lighthouse.jenkins-x.io/refs.repo"]
+	if repository == "" {
+		repository = labels["repository"]
+	}
+	branch := labels["lighthouse.jenkins-x.io/branch"]
+	if branch == "" {
+		branch = labels["branch"]
+	}
+	if build == "" {
+		buildID := labels["lighthouse.jenkins-x.io/buildNum"]
+		if buildID == "" {
+			return pr.Name
+		}
+		for _, pa := range paList {
+			if pa.Labels == nil {
+				continue
+			}
+			if pa.Labels["buildID"] == buildID {
+				if pa.Spec.Build != "" {
+					pr.Labels["build"] = pa.Spec.Build
+				}
+				return pa.Name
+			}
+		}
+		if owner != "" && repository != "" && branch != "" {
+			build = "1"
+			pr.Labels["build"] = build
+		} else {
+			return pr.Name
+		}
+	}
+	if owner != "" && repository != "" && branch != "" && build != "" {
+		return naming.ToValidName(owner + "-" + repository + "-" + branch + "-" + build)
+	}
+	return pr.Name
+}
+
+func ToPipelineActivity(pr *v1beta1.PipelineRun, pa *v1.PipelineActivity) {
 	annotations := pr.Annotations
 	labels := pr.Labels
-	pa := &v1.PipelineActivity{}
 	if pa.APIVersion == "" {
 		pa.APIVersion = "jenkins.io/v1"
 	}
@@ -88,25 +170,51 @@ func ToPipelineActivity(pr *v1beta1.PipelineRun) *v1.PipelineActivity {
 	}
 	pa.Name = pr.Name
 	pa.Namespace = pr.Namespace
-	pa.Annotations = annotations
-	pa.Labels = labels
+
+	if pa.Annotations == nil {
+		pa.Annotations = map[string]string{}
+	}
+	if pa.Labels == nil {
+		pa.Labels = map[string]string{}
+	}
+	for k, v := range annotations {
+		pa.Annotations[k] = v
+	}
+	for k, v := range labels {
+		pa.Labels[k] = v
+	}
 
 	ps := &pa.Spec
 	if labels != nil {
 		if ps.GitOwner == "" {
 			ps.GitOwner = labels["lighthouse.jenkins-x.io/refs.org"]
+			if ps.GitOwner == "" {
+				ps.GitOwner = labels["owner"]
+			}
 		}
 		if ps.GitRepository == "" {
 			ps.GitRepository = labels["lighthouse.jenkins-x.io/refs.repo"]
+			if ps.GitRepository == "" {
+				ps.GitRepository = labels["repository"]
+			}
 		}
 		if ps.GitBranch == "" {
 			ps.GitBranch = labels["lighthouse.jenkins-x.io/branch"]
+			if ps.GitBranch == "" {
+				ps.GitBranch = labels["branch"]
+			}
 		}
 		if ps.Build == "" {
-			ps.Build = labels["lighthouse.jenkins-x.io/buildNum"]
+			ps.Build = labels["build"]
+			if ps.Build == "" {
+				ps.Build = labels["lighthouse.jenkins-x.io/buildNum"]
+			}
 		}
 		if ps.Context == "" {
 			ps.Context = labels["lighthouse.jenkins-x.io/context"]
+			if ps.Context == "" {
+				ps.Context = labels["context"]
+			}
 		}
 		if ps.BaseSHA == "" {
 			ps.BaseSHA = labels["lighthouse.jenkins-x.io/baseSHA"]
@@ -122,9 +230,7 @@ func ToPipelineActivity(pr *v1beta1.PipelineRun) *v1.PipelineActivity {
 	}
 
 	podName := ""
-	running := false
-	failed := false
-	succeeded := true
+	var steps []v1.PipelineActivityStep
 	if pr.Status.TaskRuns != nil {
 		for _, v := range pr.Status.TaskRuns {
 			if v.Status == nil {
@@ -145,10 +251,8 @@ func ToPipelineActivity(pr *v1beta1.PipelineRun) *v1.PipelineActivity {
 				if terminated != nil {
 					if terminated.ExitCode == 0 {
 						status = v1.ActivityStatusTypeSucceeded
-						succeeded = true
 					} else if !terminated.FinishedAt.IsZero() {
 						status = v1.ActivityStatusTypeFailed
-						failed = true
 					}
 					started = &terminated.StartedAt
 					completed = &terminated.FinishedAt
@@ -159,7 +263,6 @@ func ToPipelineActivity(pr *v1beta1.PipelineRun) *v1.PipelineActivity {
 						status = v1.ActivityStatusTypeRunning
 					}
 					previousStepTerminated = false
-					running = true
 				}
 
 				paStep := v1.PipelineActivityStep{
@@ -174,26 +277,47 @@ func ToPipelineActivity(pr *v1beta1.PipelineRun) *v1.PipelineActivity {
 						},
 					},
 				}
-				ps.Steps = append(ps.Steps, paStep)
+				steps = append(steps, paStep)
 			}
 		}
 	}
-	if string(ps.Status) == "" {
-		if failed {
-			ps.Status = v1.ActivityStatusTypeFailed
-		} else if running {
-			ps.Status = v1.ActivityStatusTypeRunning
-		} else if succeeded {
-			ps.Status = v1.ActivityStatusTypeSucceeded
-		} else {
-			ps.Status = v1.ActivityStatusTypePending
+
+	// if the PipelineActivity has some real steps lets trust it; otherise lets merge any prevew/promote steps
+	// with steps from the PipelineRun
+	// lets add any missing steps from the PipelineActivity as they may have been created via a `jx promote` step
+	hasStep := false
+	for _, s := range ps.Steps {
+		if s.Kind == v1.ActivityStepKindTypeStage && s.Stage != nil && s.Stage.Name != "Release" {
+			hasStep = true
+			break
 		}
+	}
+	if !hasStep {
+		for _, s := range ps.Steps {
+			if s.Kind == v1.ActivityStepKindTypePreview || s.Kind == v1.ActivityStepKindTypePromote {
+				steps = append(steps, s)
+			}
+		}
+		ps.Steps = steps
+	}
+
+	if len(ps.Steps) == 0 {
+		ps.Steps = append(ps.Steps, v1.PipelineActivityStep{
+			Kind: v1.ActivityStepKindTypeStage,
+			Stage: &v1.StageActivityStep{
+				CoreActivityStep: v1.CoreActivityStep{
+					Name:   "initialising",
+					Status: v1.ActivityStatusTypeRunning,
+				},
+			},
+		})
 	}
 
 	if podName != "" {
 		pa.Labels["podName"] = podName
 	}
-	return pa
+
+	activities.UpdateStatus(pa, false, nil)
 }
 
 // Humanize splits into words and capitalises
--- pkg/common/pipelines/pipelines_test.go
+++ pkg/common/pipelines/pipelines_test.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	v1 "github.com/jenkins-x/jx-api/pkg/apis/jenkins.io/v1"
 	"github.com/jenkins-x/jx-helpers/pkg/testhelpers"
 	"github.com/jenkins-x/jx-helpers/pkg/yamls"
 	"github.com/stretchr/testify/require"
@@ -12,8 +13,8 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-func TestToPipelineActivity(t *testing.T) {
-	prFile := filepath.Join("test_data", "pipelinerun.yaml")
+func TestInitialPipelineActivity(t *testing.T) {
+	prFile := filepath.Join("test_data", "initial", "pipelinerun.yaml")
 	require.FileExists(t, prFile)
 
 	tmpDir, err := ioutil.TempDir("", "")
@@ -26,7 +27,10 @@ func TestToPipelineActivity(t *testing.T) {
 	err = yaml.Unmarshal(data, pr)
 	require.NoError(t, err, "failed to unmarshal %s", prFile)
 
-	pa := ToPipelineActivity(pr)
+	pa := &v1.PipelineActivity{}
+	ToPipelineActivity(pr, pa)
+
+	ClearTimestamps(pa)
 
 	paFile := filepath.Join(tmpDir, "pa.yaml")
 	err = yamls.SaveFile(pa, paFile)
@@ -34,5 +38,90 @@ func TestToPipelineActivity(t *testing.T) {
 
 	t.Logf("created PipelineActivity %s\n", paFile)
 
-	testhelpers.AssertTextFilesEqual(t, filepath.Join("test_data", "expected.yaml"), paFile, "generated git credentials file")
+	testhelpers.AssertTextFilesEqual(t, filepath.Join("test_data", "initial", "expected.yaml"), paFile, "generated git credentials file")
+}
+
+func TestCreatePipelineActivity(t *testing.T) {
+	prFile := filepath.Join("test_data", "create", "pipelinerun.yaml")
+	require.FileExists(t, prFile)
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "failed to create temp dir")
+
+	data, err := ioutil.ReadFile(prFile)
+	require.NoError(t, err, "failed to load %s", prFile)
+
+	pr := &v1beta1.PipelineRun{}
+	err = yaml.Unmarshal(data, pr)
+	require.NoError(t, err, "failed to unmarshal %s", prFile)
+
+	pa := &v1.PipelineActivity{}
+	ToPipelineActivity(pr, pa)
+
+	ClearTimestamps(pa)
+
+	paFile := filepath.Join(tmpDir, "pa.yaml")
+	err = yamls.SaveFile(pa, paFile)
+	require.NoError(t, err, "failed to save %s", paFile)
+
+	t.Logf("created PipelineActivity %s\n", paFile)
+
+	testhelpers.AssertTextFilesEqual(t, filepath.Join("test_data", "create", "expected.yaml"), paFile, "generated git credentials file")
+}
+
+func TestMergePipelineActivity(t *testing.T) {
+	prFile := filepath.Join("test_data", "merge", "pipelinerun.yaml")
+	require.FileExists(t, prFile)
+
+	paFile := filepath.Join("test_data", "merge", "pa.yaml")
+	require.FileExists(t, prFile)
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err, "failed to create temp dir")
+
+	pr := &v1beta1.PipelineRun{}
+	err = yamls.LoadFile(prFile, pr)
+	require.NoError(t, err, "failed to load %s", prFile)
+
+	pa := &v1.PipelineActivity{}
+	err = yamls.LoadFile(paFile, pa)
+	require.NoError(t, err, "failed to load %s", paFile)
+
+	ToPipelineActivity(pr, pa)
+
+	ClearTimestamps(pa)
+
+	paFile = filepath.Join(tmpDir, "pa.yaml")
+	err = yamls.SaveFile(pa, paFile)
+	require.NoError(t, err, "failed to save %s", paFile)
+
+	t.Logf("created PipelineActivity %s\n", paFile)
+
+	testhelpers.AssertTextFilesEqual(t, filepath.Join("test_data", "merge", "expected.yaml"), paFile, "generated git credentials file")
+}
+
+func ClearTimestamps(pa *v1.PipelineActivity) {
+	pa.Spec.StartedTimestamp = nil
+	pa.Spec.CompletedTimestamp = nil
+	for i := range pa.Spec.Steps {
+		step := &pa.Spec.Steps[i]
+		if step.Stage != nil {
+			step.Stage.StartedTimestamp = nil
+			step.Stage.CompletedTimestamp = nil
+
+			for j := range step.Stage.Steps {
+				s2 := &step.Stage.Steps[j]
+				s2.StartedTimestamp = nil
+				s2.CompletedTimestamp = nil
+			}
+		}
+		if step.Promote != nil {
+			step.Promote.StartedTimestamp = nil
+			step.Promote.CompletedTimestamp = nil
+		}
+		if step.Preview != nil {
+			step.Preview.StartedTimestamp = nil
+			step.Preview.CompletedTimestamp = nil
+		}
+	}
 }
