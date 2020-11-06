dadfcbcdc3e2de269432e4a39e6dc9911cafa16b
James Strachan <james.strachan@gmail.com>
2020-11-06 13:22:32 +0000

    fix: the boot jobs view

--- promote.sh
+++ promote.sh
@@ -2,3 +2,4 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
+jx step create pr regex --regex '\s+OctantJXVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/versions.go --repo https://github.com/jenkins-x/jx-cli.git
